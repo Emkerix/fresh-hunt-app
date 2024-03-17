b34b31dfe04399136d61ad6b715bc7f82aefb95b
Emkerix <michalekrx8@gmail.com>
2024-03-17 18:15:34 +0100

    feat: add swagger documentation setup

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import routers from './routes';
 import { databaseConnection } from './config/connection';
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 
 databaseConnection;
 
@@ -14,6 +16,27 @@ app.use(express.json());
 const APP_PORT = process.env.APP_PORT || 3001;
 
 app.use('/api', routers);
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Library API',
+      version: '1.0.0',
+      description: 'Express Library API'
+    },
+    servers: [
+      {
+        url: `${process.env.BASE_URL}:${APP_PORT}`
+      }
+    ]
+  },
+  apis: ['./routes/**/index.ts']
+};
+
+const specs = swaggerJsDoc(options);
+
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(specs));
 
 app.get('/', (request, response, next) => {
   response.json({ status: 'running' });
